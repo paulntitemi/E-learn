c8d5e47544378a4a116a046b36d6dca0c5a21fb6
gr1nch3 <lloydm763@gmail.com>
2022-08-22 01:30:15 +0000

    Created data models

---
+++ lib/data/course/course_json.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// subject list
+const List subjects = [
+  {"title": "Physics", "img": "#"},
+  {"title": "Maths", "img": "#"},
+  {"title": "Chemistry", "img": "#"},
+  {"title": "Language Arts", "img": "#"},
+];
+
+// explore category list
+const List exploreCategory = ["Revision Lectures", "Recommended Lectures"];
+
+// video category list
+const List videoCategory = [
+  "Best of Physics",
+  "best of Chemistry",
+  "Best of Mathematics",
+  "Best of Biology",
+  "Best of Geography",
+  "Best of Language Arts"
+];
+
+// course list data model
+const List courses = [
+  {
+    "img": "#",
+    "title": "#",
+    "short_description": "#",
+    "lond_description": "#",
+    "duration": "#",
+    "video_url": "#",
+    "topics": [
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+      {"title": "#", "duration": "#"},
+    ]
+  },
+];
