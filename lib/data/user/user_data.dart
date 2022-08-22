bb209429279b960d78830734983d1dea2b4845a2
gr1nch3 <lloydm763@gmail.com>
2022-08-22 21:53:02 +0000

    Created user data model

---
+++ lib/data/user/user_data.dart
@@ -0,0 +1,10 @@
+const List user = [
+  {
+    "image": "#",
+    "firstname": "Akshay",
+    "lastname": "Maury",
+    "occupation": "student",
+    "local_rank": "1",
+    "global_rank": "3",
+  },
+];
