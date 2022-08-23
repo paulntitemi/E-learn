2842191a283dd14038e4840669d23a1416acc13a
gr1nch3 <lloydm763@gmail.com>
2022-08-23 02:12:26 +0000

    made beta explore page

--- lib/presentation/pages/explore.dart
+++ lib/presentation/pages/explore.dart
@@ -1,4 +1,6 @@
+import 'package:elearn/data/course/course_json.dart';
 import 'package:elearn/presentation/themes/colors.dart';
+import 'package:elearn/presentation/widget/course_widget.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_icons/flutter_icons.dart' show Feather;
 
@@ -12,6 +14,95 @@ class ExplorePage extends StatefulWidget {
 class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: background,
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        // courses and the category for the explore page
+        children: List.generate(exploreCategory.length, (index) {
+          return Padding(
+            padding: const EdgeInsets.only(
+              right: 10,
+              top: 12,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 8,
+                right: 8,
+                top: 8,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // category widget
+                children: [
+                  // category name
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, bottom: 5, top: 2),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        // row for design container and category name
+                        Expanded(
+                          child: Row(
+                            children: [
+                              // design Container
+                              Container(
+                                width: 3,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                    color: primary,
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              // category name
+                              Text(
+                                exploreCategory[index]['title'],
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    color: black,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // view all chevron
+                        const Icon(
+                          Icons.chevron_right,
+                          color: primary,
+                        ),
+                      ],
+                    ),
+                  ),
+                  // a little space?
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  // courses in category
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List.generate(5, (index) {
+                        // five items only
+                        return CourseView(
+                          desc: courses[index]['short_description'],
+                        );
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
