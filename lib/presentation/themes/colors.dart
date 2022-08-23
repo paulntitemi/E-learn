64712b59cc4a5f1df517d5f0d9bd87ac17427b88
gr1nch3 <lloydm763@gmail.com>
2022-08-23 02:13:30 +0000

    some fixes

--- lib/presentation/themes/colors.dart
+++ lib/presentation/themes/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const Color primary = Color(0xFF1E88E5);
 const Color black = Color(0xFF000000);
 const Color shadow = Color(0xFFD6D6D6);
-const Color background = Color(0xFFE0E0E0);
+const Color background = Color(0xB3FFFFFF);
 const Color white = Color(0xFFFFFFFF);
 const Color disabled = Color(0xFF5f5f5f);
+// const Color background = Color(0x62FFFFF);
