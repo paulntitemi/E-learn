c97938bb2383d798e7e73be8ec16cfbc17b54835
gr1nch3 <lloydm763@gmail.com>
2022-08-24 08:03:39 +0000

    separated category data from course json

---
+++ lib/data/course/categories.dart
@@ -0,0 +1,38 @@
+// explore category list
+const List exploreCategory = [
+  {"title": "Revision Lectures"},
+  {"title": "Recommended Lectures"},
+  {"title": "Revision Lectures"},
+  {"title": "Recommended Lectures"},
+  {"title": "Revision Lectures"},
+  {"title": "Recommended Lectures"},
+  {"title": "Revision Lectures"},
+  {"title": "Recommended Lectures"},
+];
+
+// video category list
+const List videoCategory = [
+  {"title": "Best of Physics"},
+  // {"title": "Intro to physics"},
+  // {"title": "Everything Physics"},
+  // {"titles": "Physics for Dummies"},
+  {"title": "best of Chemistry"},
+  {"title": "Best of Mathematics"},
+  {"title": "Best of Biology"},
+  {"title": "Best of Geography"},
+  {"title": "Best of Language Arts"},
+];
+
+// planner category
+const List plannerCategory = [
+  {"title": "Today's work"},
+  {"title": "Calendar"},
+  // {"title": "Events"},
+];
+
+// leaderboard category
+const List leaderboardCategory = [
+  {"title": "Facebook Leaderboard", "keyword": "facebook"},
+  {"title": "Twitter Leaderboard", "keyword": "twitter"},
+  {"title": "Instagram Leaderboard", "keyword": "instagram"},
+];
