afde8fbfda6ab668578b7132b3379fc8fabda1ee
gr1nch3 <lloydm763@gmail.com>
2022-08-25 08:46:41 +0000

    data for leaderboardpage

---
+++ lib/data/course/leaderboard.dart
@@ -0,0 +1,6 @@
+const List leaders = [
+  {"rank": "1", "username": "Esther Yaa", "score": "3000"},
+  {"rank": "2", "username": "kwame W.", "score": "2000"},
+  {"rank": "3", "username": "Mark A.", "score": "1000"},
+  {"rank": "4", "username": "A. Samuels", "score": "500"},
+];
