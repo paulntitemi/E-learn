7ad83f74ba00687309934f7003b445c37cf12dce
gr1nch3 <lloydm763@gmail.com>
2022-08-28 23:30:28 +0000

    added player widget

--- lib/utils/player_util.dart
+++ lib/utils/player_util.dart
@@ -1,9 +1,16 @@
-import 'package:flutter/material.dart';
+import 'package:elearn/presentation/themes/colors.dart';
 import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class CourseDetail extends StatefulWidget {
-  const CourseDetail({Key? key}) : super(key: key);
+  final String videoUrl;
+  final List titles;
+  const CourseDetail({
+    Key? key,
+    required this.videoUrl,
+    required this.titles,
+  }) : super(key: key);
 
   @override
   State<CourseDetail> createState() => _CourseDetailState();
@@ -12,12 +19,15 @@ class CourseDetail extends StatefulWidget {
 class _CourseDetailState extends State<CourseDetail> {
   // Flick player utils
   late FlickManager flickManager;
+  // Url manager
+  late String changingVideoUrl = widget.videoUrl;
+  // late List courseTopics = widget.titles;
 
   @override
   void initState() {
     super.initState();
     flickManager = FlickManager(
-      videoPlayerController: VideoPlayerController.asset('url'),
+      videoPlayerController: VideoPlayerController.asset(widget.videoUrl),
     );
   }
 
@@ -29,10 +39,107 @@ class _CourseDetailState extends State<CourseDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int selected = 0;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: background,
+        body: Column(
+          children: [
+            // player
+            getPlayer(),
+            // pdf download
+            const SizedBox(
+              height: 20,
+            ),
+            // topics within the course
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Expanded(
+                child: Column(
+                  children: List.generate(
+                    widget.titles.length,
+                    (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 5,
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(
+                              () {
+                                selected = index;
+                                flickManager = FlickManager(
+                                  videoPlayerController:
+                                      VideoPlayerController.asset(
+                                          widget.titles[index]['video_url']),
+                                );
+                              },
+                            );
+                          },
+                          child: Container(
+                            height: 60,
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 8,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  // username and rank numner
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      // number
+                                      // a little space
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      // username
+                                      Text(
+                                        widget.titles[index]['title'],
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                          color: black,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              color: selected == index ? primary : white,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: shadow,
+                                  blurRadius: 10,
+                                  offset: Offset(0, 2),
+                                  spreadRadius: 3, // Shadow position
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget getPlayer() {
+    // ignore: avoid_unnecessary_containers
     return Container(
       child: FlickVideoPlayer(flickManager: flickManager),
     );
