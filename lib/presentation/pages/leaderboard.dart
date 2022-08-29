441593d49c9e84c6689bbfc4a361f8312d9684ec
gr1nch3 <lloydm763@gmail.com>
2022-08-29 03:16:32 +0000

    added custom appbar

--- lib/presentation/pages/leaderboard.dart
+++ lib/presentation/pages/leaderboard.dart
@@ -2,6 +2,8 @@ import 'package:elearn/data/course/categories.dart';
 import 'package:elearn/data/course/leaderboard.dart';
 import 'package:elearn/presentation/themes/colors.dart';
 import 'package:elearn/presentation/widget/misc.dart';
+import 'package:elearn/data/user/user_data.dart';
+import 'package:elearn/presentation/widget/subjects_widget.dart';
 import 'package:flutter/material.dart';
 
 class LeaderBoardPage extends StatefulWidget {
@@ -16,7 +18,107 @@ class _LeaderBoardPageState extends State<LeaderBoardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: background,
+      appBar: getAppBar(),
       body: getBody(),
+    );
+  }
+
+  // Custom appbar
+  PreferredSizeWidget getAppBar() {
+    // grow the app bar to fit subject list
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(200),
+      child: Container(
+        // duration: const Duration(milliseconds: 500),
+        // curve: Curves.fastOutSlowIn,
+        // Appbar container
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 12,
+            right: 12,
+          ),
+          child: Column(
+            // Colums conataining the username, profilepic, and the searchbar
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                // user name and searchbar
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Hi, ' + user[0]['firstname'],
+                      style: const TextStyle(
+                          color: black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    child: CircleAvatar(
+                      radius: 25.0,
+                      backgroundColor: white,
+                      foregroundImage: AssetImage(user[0]['image']),
+                    ),
+                  )
+                ],
+              ),
+              // search bar
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(12),
+                    isDense: true,
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: shadow),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    filled: true,
+                    fillColor: white,
+                    hintStyle: const TextStyle(color: disabled),
+                    hintText: "Search",
+                    suffixIcon: const Icon(
+                      Icons.search,
+                      color: disabled,
+                      size: 25,
+                    ),
+                  ),
+                ),
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: shadow,
+                      blurRadius: 8,
+                      offset: Offset(0, 1),
+                    ),
+                  ],
+                ),
+              ),
+
+              // tab for leaderboard
+              const LeaderBoardTab(),
+            ],
+          ),
+        ),
+        // appbar container Decoration
+        decoration: const BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: shadow,
+              blurRadius: 10,
+              offset: Offset(0, 8),
+              spreadRadius: 1, // Shadow position
+            ),
+          ],
+        ),
+      ),
     );
   }
 
