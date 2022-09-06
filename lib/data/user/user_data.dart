65fce5e203c56b267300ebee6a8150d41901e260
gr1nch3 <lloydm763@gmail.com>
2022-09-06 14:48:43 +0000

    minor tweak

--- lib/data/user/user_data.dart
+++ lib/data/user/user_data.dart
@@ -4,6 +4,7 @@ const List user = [
     "firstname": "Akshay",
     "lastname": "Maury",
     "occupation": "student",
+    "email": "example@email.com",
     "local_rank": "1",
     "global_rank": "3",
   },
