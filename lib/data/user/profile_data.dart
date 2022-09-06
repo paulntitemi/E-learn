f7b1ea7395e3659ca355483e5a5e3f05f6958437
gr1nch3 <lloydm763@gmail.com>
2022-09-06 14:47:04 +0000

    data for user profile page

---
+++ lib/data/user/profile_data.dart
@@ -0,0 +1,44 @@
+const List preferences = [
+  {
+    "category_title": "Video Preferences",
+    "options": [
+      {
+        "title": "Download options",
+      },
+      {
+        "title": "Video playback options",
+      },
+      {
+        "title": "Your download courses",
+      },
+    ]
+  },
+  {
+    "category_title": "Account settings",
+    "options": [
+      {
+        "title": "Notifications",
+      },
+      {
+        "title": "Account security",
+      },
+      {
+        "title": "Close Account",
+      },
+    ]
+  },
+  {
+    "category_title": "Support",
+    "options": [
+      {
+        "title": "About E-learn",
+      },
+      {
+        "title": "Help and support",
+      },
+      {
+        "title": "Share E-learn",
+      },
+    ]
+  },
+];
